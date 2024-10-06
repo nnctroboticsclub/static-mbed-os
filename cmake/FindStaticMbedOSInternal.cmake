e7bc8f47db379abe74144ce46295452c18d2f201
syoch <syoch64@gmail.com>
2024-10-06 10:51:54 +0000

    Update: Fix some

--- cmake/FindStaticMbedOSInternal.cmake
+++ cmake/FindStaticMbedOSInternal.cmake
@@ -1,40 +1,4 @@
-# ----- dumper
-
-# Get all propreties that cmake supports
-if(NOT CMAKE_PROPERTY_LIST)
-    execute_process(COMMAND cmake --help-property-list OUTPUT_VARIABLE CMAKE_PROPERTY_LIST)
-
-    # Convert command output into a CMake list
-    string(REGEX REPLACE ";" "\\\\;" CMAKE_PROPERTY_LIST "${CMAKE_PROPERTY_LIST}")
-    string(REGEX REPLACE "\n" ";" CMAKE_PROPERTY_LIST "${CMAKE_PROPERTY_LIST}")
-    list(REMOVE_DUPLICATES CMAKE_PROPERTY_LIST)
-endif()
-
-function(print_properties)
-    message("CMAKE_PROPERTY_LIST = ${CMAKE_PROPERTY_LIST}")
-endfunction()
-
-function(print_target_properties target)
-    if(NOT TARGET ${target})
-      message(STATUS "There is no target named '${target}'")
-      return()
-    endif()
-
-    foreach(property ${CMAKE_PROPERTY_LIST})
-        string(REPLACE "<CONFIG>" "${CMAKE_BUILD_TYPE}" property ${property})
-
-        # Fix https://stackoverflow.com/questions/32197663/how-can-i-remove-the-the-location-property-may-not-be-read-from-target-error-i
-        if(property STREQUAL "LOCATION" OR property MATCHES "^LOCATION_" OR property MATCHES "_LOCATION$")
-            continue()
-        endif()
-
-        get_property(was_set TARGET ${target} PROPERTY ${property} SET)
-        if(was_set)
-            get_target_property(value ${target} ${property})
-            message("${target} ${property} = ${value}")
-        endif()
-    endforeach()
-endfunction()
+include(TargetDumper)
 
 # ----- Properties
 
@@ -59,13 +23,16 @@ file(READ
   ${StaticMbedOSInternalRoot}/includes.txt
   STATIC_MBED_OS_INCLUDES
 )
-string(REPLACE "\n" ";" STATIC_MBED_OS_INCLUDES ${STATIC_MBED_OS_INCLUDES})
+foreach(dir ${STATIC_MBED_OS_INCLUDES})
+  if(NOT EXISTS ${dir})
+    list(REMOVE_ITEM STATIC_MBED_OS_INCLUDES ${dir})
+  endif()
+endforeach()
 
 file(READ
   ${StaticMbedOSInternalRoot}/compile-options.txt
   STATIC_MBED_OS_COMPILE_OPTIONS
 )
-string(REPLACE "\n" ";" STATIC_MBED_OS_COMPILE_OPTIONS ${STATIC_MBED_OS_COMPILE_OPTIONS})
 
 file(READ
   ${StaticMbedOSInternalRoot}/link-libraries.txt
